d542814257f8585b8732cbe1481f0fe1bbf1a6e1
Keith Tom <keith.tom@gmail.com>
2011-10-17 13:54:51 -0700

    specs caught this.

--- lib/galaxy/email.rb
+++ lib/galaxy/email.rb
@@ -1,11 +1,11 @@
 module Galaxy
   class Email < Galaxy::Base
-    def self.invite(from, to, emails, msg=nil)
-      post(:invite, :from => from, :to => to, :emails => emails, :msg => msg)
+    def self.invite(from, to_emails, msg=nil)
+      post(:invite, :from => from, :emails => to_emails, :msg => msg)
     end
 
-    def self.recommend_deal(from, to, deal_id, msg=nil)
-      post(:recommend_deal, :from => from, :to => to, :deal_id => deal_id, :msg => msg)
+    def self.recommend_deal(from, to_emails, deal_id, msg=nil)
+      post(:recommend_deal, :from => from, :emails => to_emails, :deal_id => deal_id, :msg => msg)
     end
   end
 end

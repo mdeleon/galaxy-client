df2e9e820e7e58f69118d782927576833160009b
Keith Tom <keith.tom@gmail.com>
2011-11-10 14:06:07 -0800

    update Galaxy::Email.invite to be more consistent with other calls
    - changed signature to a params signature
    - rescue exceptions and wrap in active resource invalid error

--- lib/galaxy/email.rb
+++ lib/galaxy/email.rb
@@ -2,15 +2,18 @@ module Galaxy
   # The Email resource is responsible for creating transactional emails through Galaxy.
   class Email < Galaxy::Base
 
-    # Sends an invite email.
-    # @param [String] from
-    #   An email address to be used as the from address.
-    # @param [Array] to_emails
-    #   An array of email address strings.
-    # @param [String] msg
-    #   An optional message to provide to customize the invite email.
-    def self.invite(from, to_emails, msg=nil)
-      post(:invite, :from => from, :emails => to_emails, :msg => msg)
+    # Sends an invite email to multiple email address.
+    # @param [Hash] params
+    #   :user_id is the id of the user that we send the invitations on behalf of.
+    #   :emails is a comma-seperated string of email addresses.
+    #   :message is the text to customize the invite email.
+    def self.invite(params={})
+      params.assert_valid_keys(:user_id, :emails, :message)
+      post(:invite, params)
+    rescue ActiveResource::ResourceInvalid => e
+      instance = new(params)
+      instance.load_remote_errors(e)
+      raise ActiveResource::ResourceInvalid.new(instance)
     end
 
     def self.recommend_deal(from, to_emails, deal_id, msg=nil)

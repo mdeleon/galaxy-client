d582d380fea366fbfd1b10225c7a01cf02c38e47
Keith Tom <keith.tom@gmail.com>
2012-01-25 22:26:51 -0800

    add link/ unlink

--- lib/galaxy/card_link.rb
+++ lib/galaxy/card_link.rb
@@ -3,5 +3,13 @@ module Galaxy
     def self.linked(deal_id, user_id)
       find(:first, :from => "/api/v2/card_links/linked.json", :params => { :deal_id => deal_id, :user_id => user_id})
     end
+
+    def link
+      put :link
+    end
+
+    def unlink
+      put :unlink
+    end
   end
 end

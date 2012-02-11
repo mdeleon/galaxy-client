e37418d8a13b618094cd0d44d99cc5c35ed69501
Brian Woods <brian@anythingsocial.com>
2012-02-10 17:25:20 -0800

    rename address to to_s

--- lib/galaxy/address.rb
+++ lib/galaxy/address.rb
@@ -1,6 +1,6 @@
 module Galaxy
   class Address < Galaxy::Base
-    def address
+    def to_s
       output =  [street, locality, region].compact.join(', ')
       output << " #{ postal_code ? postal_code[0..4] : '' }"
       output.strip

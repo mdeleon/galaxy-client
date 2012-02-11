98c99560475daaf42f5270167f2587657d43757f
Brian Woods <brian@anythingsocial.com>
2012-02-10 17:21:10 -0800

    adding address method

--- lib/galaxy/address.rb
+++ lib/galaxy/address.rb
@@ -1,4 +1,9 @@
 module Galaxy
   class Address < Galaxy::Base
+    def address
+      output =  [street, locality, region].compact.join(', ')
+      output << " #{ postal_code ? postal_code[0..4] : '' }"
+      output.strip
+    end
   end
 end

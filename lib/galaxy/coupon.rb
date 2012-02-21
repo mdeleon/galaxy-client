de222dcb3d1a7febedab8dd2afabe31a32fad6bb
zellunit <matt@homerun.com>
2012-02-21 13:07:01 -0800

    coupon magic for keith

--- lib/galaxy/coupon.rb
+++ lib/galaxy/coupon.rb
@@ -1,11 +1,21 @@
 module Galaxy
   class Coupon < Galaxy::Base
-    def redeem
-      put(:redeem)
+    def redeem(params={})
+      begin
+        put(:redeem, params)
+      rescue ActiveResource::ResourceInvalid => e
+        instance = self.class.new(params)
+        instance.load_remote_errors(e)
+        raise ActiveResource::ResourceInvalid.new(instance)
+      end
     end
 
     def deal
       @deal ||= model_for(:deal).find(self.deal_id)
+    end
+
+    def self.find_by_barcode(barcode)
+      find(:all, from: "/api/v2/coupons/find_by_barcode.json", params: {:barcode => barcode})
     end
   end
 end

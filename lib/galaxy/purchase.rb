51936c3a9c6281be83cf854145f184bc1e758ad7
zellunit <matt@homerun.com>
2012-02-21 19:39:42 -0800

    slight modification to purchase

--- lib/galaxy/purchase.rb
+++ lib/galaxy/purchase.rb
@@ -9,5 +9,9 @@ module Galaxy
     def deal
       @deal ||= model_for(:deal).find(self.deal_id)
     end
+
+    def credit_card
+      @credit_card ||= model_for(:credit_card).find(self.credit_card_id)
+    end
   end
 end

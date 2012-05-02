abe3d983195a56ae4f1f212479dbbc274e7b6919
James Way <jamescway@gmail.com>
2012-05-01 19:30:15 -0700

    create card link specs

---
+++ spec/galaxy/card_link_spec.rb
@@ -0,0 +1,33 @@
+require File.expand_path('../../spec_helper', __FILE__)
+require "galaxy/card_link"
+
+describe Galaxy::CardLink do
+
+  describe "#link" do
+    it "sends GET to /card_links/:id/link.json" do
+      cardlinks = [{ :id => "d9c3k19d" }]
+      cardlink = Galaxy::CardLink.new(:id => "d02k49d")
+      mock_galaxy(:put, "/api/v2/card_links/#{cardlink.id}/link.json", get_headers, cardlinks.to_json, 200)
+
+      cardlink = Galaxy::CardLink.new(:id => cardlink.id)
+      response = cardlink
+      response.should be_instance_of(Array)
+      response.first.should be_instance_of(Galaxy::CardLink)
+      response.first.id.should ==  "d9c3k19d"
+    end
+  end
+
+  describe "#unlink" do
+    it "sends GET to /card_links/:id/unlink.json" do
+      cardlinks = [{ :id => "d9c3k19d" }]
+      cardlink = Galaxy::CardLink.new(:id => "d02k49d")
+      mock_galaxy(:put, "/api/v2/card_links/#{cardlink.id}/unlink.json", get_headers, cardlinks.to_json, 200)
+
+      cardlink = Galaxy::CardLink.new(:id => cardlink.id)
+      response = cardlink
+      response.should be_instance_of(Array)
+      response.first.should be_instance_of(Galaxy::CardLink)
+      response.first.id.should ==  "d9c3k19d"
+    end
+  end
+end

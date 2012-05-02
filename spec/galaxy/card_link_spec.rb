00ccfe72b9f0ee5c7f6660963c34f7962bc65a84
James Way <jamescway@gmail.com>
2012-05-02 16:43:28 -0700

    card_link revamped after refactoring, because it wasn't testing anything

--- spec/galaxy/card_link_spec.rb
+++ spec/galaxy/card_link_spec.rb
@@ -3,23 +3,23 @@ require "galaxy/card_link"
 
 describe Galaxy::CardLink do
   let(:cardlink_id) { "d9c3k19d" }
-  let(:cardlinks) { [{ :id => cardlink_id }] }
+  let(:linked_cardlink)   { { :linked => true } }
+  let(:unlinked_cardlink) { { :linked => false } }
+  let(:cardlink) { Galaxy::CardLink.new(:id => cardlink_id) }
 
   describe "#link" do
-    it "sends GET to /card_links/:id/link.json" do
-      mock_galaxy(:put, "/api/v2/card_links/#{cardlink_id}/link.json", get_headers, cardlinks.to_json, 200)
-      response = Galaxy::CardLink.new(:id => cardlink_id)
-      response.should be_instance_of(Galaxy::CardLink)
-      response.id.should == cardlink_id
+    it "sends PUT to /card_links/:id/link.json" do
+      mock_galaxy(:put, "/api/v2/card_links/#{cardlink_id}/link.json", post_headers, linked_cardlink.to_json, 200)
+      cl = cardlink.link
+      cl.linked.should eq true
     end
   end
 
   describe "#unlink" do
-    it "sends GET to /card_links/:id/unlink.json" do
-      mock_galaxy(:put, "/api/v2/card_links/#{cardlink_id}/unlink.json", get_headers, cardlinks.to_json, 200)
-      response = Galaxy::CardLink.new(:id => cardlink_id)
-      response.should be_instance_of(Galaxy::CardLink)
-      response.id.should == cardlink_id
+    it "sends PUT to /card_links/:id/unlink.json" do
+      mock_galaxy(:put, "/api/v2/card_links/#{cardlink_id}/unlink.json", post_headers, unlinked_cardlink.to_json, 200)
+      cl = cardlink.unlink
+      cl.linked.should eq false
     end
   end
 end

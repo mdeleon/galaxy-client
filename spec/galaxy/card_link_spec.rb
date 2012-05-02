c9cf371891fd4cb0833476f96507417c3ccd4b37
James Way <jamescway@gmail.com>
2012-05-02 15:48:40 -0700

    dry up specs some more

--- spec/galaxy/card_link_spec.rb
+++ spec/galaxy/card_link_spec.rb
@@ -2,24 +2,25 @@ require File.expand_path('../../spec_helper', __FILE__)
 require "galaxy/card_link"
 
 describe Galaxy::CardLink do
-  let(:cardlinks) { [{ :id => "d9c3k19d" }] }
-  let(:cardlink)  { Galaxy::CardLink.new(:id => "d9c3k19d") }
+  let(:cardlink_id) { "d9c3k19d" }
+  let(:cardlinks) { [{ :id => cardlink_id }] }
+  let(:cardlink)  { Galaxy::CardLink.new(:id => cardlink_id) }
 
   describe "#link" do
     it "sends GET to /card_links/:id/link.json" do
-      mock_galaxy(:put, "/api/v2/card_links/#{cardlink.id}/link.json", get_headers, cardlinks.to_json, 200)
-      response = Galaxy::CardLink.new(:id => cardlink.id)
+      mock_galaxy(:put, "/api/v2/card_links/#{cardlink_id}/link.json", get_headers, cardlinks.to_json, 200)
+      response = Galaxy::CardLink.new(:id => cardlink_id)
       response.should be_instance_of(Galaxy::CardLink)
-      response.id.should == "d9c3k19d"
+      response.id.should == cardlink_id
     end
   end
 
   describe "#unlink" do
     it "sends GET to /card_links/:id/unlink.json" do
-      mock_galaxy(:put, "/api/v2/card_links/#{cardlink.id}/unlink.json", get_headers, cardlinks.to_json, 200)
-      response = Galaxy::CardLink.new(:id => cardlink.id)
+      mock_galaxy(:put, "/api/v2/card_links/#{cardlink_id}/unlink.json", get_headers, cardlinks.to_json, 200)
+      response = Galaxy::CardLink.new(:id => cardlink_id)
       response.should be_instance_of(Galaxy::CardLink)
-      response.id.should == "d9c3k19d"
+      response.id.should == cardlink_id
     end
   end
 end

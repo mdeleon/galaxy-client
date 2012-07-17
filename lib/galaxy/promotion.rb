feac6105e4b5151afad29c4091a07c6ea637fe08
charleschu <charles_chu@foxmail.com>
2012-07-16 17:33:50 -0700

    fix the description message for promotion

--- lib/galaxy/promotion.rb
+++ lib/galaxy/promotion.rb
@@ -2,7 +2,7 @@ module Galaxy
   class Promotion < Galaxy::Base
     # Apply a promotion to user
     # @param [Hash] params
-    #   :user_id is the id of the user that we send the invitations on behalf of.
+    #   :user_id is the id of the user that we will apply the promotion for.
     def apply_to_user(params = {})
       params.assert_valid_keys(:user_id)
       post(:apply_to_user, params)

427b616980a3b8a1dd78c80586948c8ea8218dc4
Oliver Wang <yuesefa@gmail.com>
2012-08-27 14:47:38 -0700

    incase the location only has a phone number

--- spec/galaxy/location_spec.rb
+++ spec/galaxy/location_spec.rb
@@ -2,7 +2,24 @@ require File.expand_path('../../spec_helper', __FILE__)
 require "galaxy/location"
 
 describe Galaxy::Location do
-  subject { Galaxy::Location.new :created_at => nil }
+  subject { Galaxy::Location.new :created_at => nil,
+                                 :address_id => nil}
   
   it_timeifies :created_at
+  
+  describe "location" do
+    context "address_id is nil" do
+      it "returns nil" do
+        subject.address.should be_nil
+      end
+    end
+    
+    context "address_id exists" do
+      it "return the address" do
+        subject.address_id = "abc"
+        subject.stub_chain(:model_for, :find).and_return :address
+        subject.address.should_not be_nil
+      end
+    end
+  end
 end

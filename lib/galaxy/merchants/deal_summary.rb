feca83f54dd2a0d9080a88a4fa58a958c184b7ef
Yi Wen <ywen.github@gmail.com>
2012-10-08 18:34:55 -0500

    Revert "Use path"
    This reverts commit 7c2a39668387d4ff71e5f36bdb063b0678d39734.

--- lib/galaxy/merchants/deal_summary.rb
+++ lib/galaxy/merchants/deal_summary.rb
@@ -3,8 +3,7 @@ module Galaxy
     class DealSummary < Galaxy::Base
       class << self
         def summary(params)
-          #due to the implementation of ActiveResource, path is necessary
-          find :one, from: "/#{path}/merchants/#{params[:merchant_id]}/deal_summaries.json"
+          find :one, from: "#{self.site}merchants/#{params[:merchant_id]}/deal_summaries.json"
         end
       end
     end
--- spec/galaxy/merchants/deal_summary_spec.rb
+++ spec/galaxy/merchants/deal_summary_spec.rb
@@ -6,7 +6,7 @@ module Galaxy
       describe ".summary" do
         it "finds the singleton resource" do
           DealSummary.should_receive(:find).with(
-            :one, from: "/#{Galaxy::Base.path}/merchants/merchant-id/deal_summaries.json"
+            :one, from: "#{Galaxy::Base.site}merchants/merchant-id/deal_summaries.json"
           )
           DealSummary.summary(merchant_id: "merchant-id")
         end

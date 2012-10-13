b0192884465216864f4a96ae495b2fbd97ec1441
Zac Horn <zghorn@gmail.com>
2012-10-13 12:55:41 -0700

    configure rspec

--- tasks/rspec.rb
+++ tasks/rspec.rb
@@ -22,11 +22,6 @@ begin
     # config.mock_with :rr
     config.mock_with :rspec
 
-    # If true, the base class of anonymous controllers will be inferred
-    # automatically. This will be the default behavior in future versions of
-    # rspec-rails.
-    config.infer_base_class_for_anonymous_controllers = false
-
     config.extend Macros::Controller, :type => :controller
     config.extend Macros::System
   end
